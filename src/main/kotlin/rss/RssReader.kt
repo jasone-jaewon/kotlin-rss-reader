9c13d32cd56628e26b094a2ee49fcaf52f8e0586
hyunaprm <sohyua2@naver.com>
2024-09-23 14:58:22 +0900

    feat: RssRead 함수 구현

--- src/main/kotlin/rss/RssReader.kt
+++ src/main/kotlin/rss/RssReader.kt
@@ -1,9 +1,19 @@
 package rss
 
+import org.w3c.dom.Element
 import rss.model.RssNodes
+import javax.xml.parsers.DocumentBuilderFactory
 
 object RssReader {
+
+    private const val FIRST_INDEX = 0
+    private const val CHANNEL_TAG_NAME = "channel"
+    private const val ITEM_TAG_NAME = "item"
     fun read(url: String): RssNodes {
-        TODO()
+        val factory = DocumentBuilderFactory.newInstance()
+        val xml = factory.newDocumentBuilder().parse(url)
+        val channel = xml.getElementsByTagName(CHANNEL_TAG_NAME).item(FIRST_INDEX) as Element
+        val itemNodes = channel.getElementsByTagName(ITEM_TAG_NAME)
+        return RssNodes(itemNodes)
     }
 }
