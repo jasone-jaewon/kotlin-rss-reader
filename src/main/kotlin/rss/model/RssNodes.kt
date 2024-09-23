af62513dae46a28af411e8264f14226137e922c1
hyunaprm <sohyua2@naver.com>
2024-09-23 15:11:00 +0900

    feat: toPost 구현

--- src/main/kotlin/rss/model/RssNodes.kt
+++ src/main/kotlin/rss/model/RssNodes.kt
@@ -1,8 +1,26 @@
 package rss.model
 
+import org.w3c.dom.Element
 import org.w3c.dom.NodeList
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class RssNodes(
     private val nodes: NodeList
 ) {
+
+    fun toPosts(): List<Post> {
+        return (0 until nodes.length).map { i ->
+            val item = nodes.item(i) as Element
+            val title = item.getElementsByTagName("title").item(0).textContent
+            val link = item.getElementsByTagName("link").item(0).textContent
+            val createdBy = item.getElementsByTagName("dc:creator").item(0).textContent
+            val createdAt = item.getElementsByTagName("pubDate").item(0).textContent
+
+            val formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss Z")
+            val at = ZonedDateTime.parse(createdAt, formatter)
+
+            Post(title, link, at, createdBy)
+        }
+    }
 }
